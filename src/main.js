a21e8d0c0c12ddd1b8fae6349b31d560c66641a3
Jerry-Hong <s6323859@gmail.com>
2017-01-02 21:33:03 +0800

    完成 非同步 資料存取

--- src/main.js
+++ src/main.js
@@ -5,25 +5,67 @@
     const todoListDOM = document.getElementById('todoList');
 
     // Modle
-    const todoList = [];
+    let todoList = [];
+
+    // 取得 api 資料
+
+    fetch('http://localhost:3000/todolist')
+    .then(res => res.json())
+    .then(json => {
+        todoList = todoList.concat(json);
+        render(todoList);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
 
     // 建立 todo item 的
     const newItem = value => ({ name: value, isComplete: false })
 
     // 新增、修改、刪除三個函式，具有 sideEffect => render
     const addItem = item => { 
-        todoList.push(item); 
-        render(todoList);
+        // todoList.push(item);
+        fetch('http://localhost:3000/todolist', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(item)
+        })
+        .then(res => res.json())
+        .then(json => {
+            todoList.push(json);
+            render(todoList);
+        });
     }
-    const toggleItem = (index) => {
-        todoList[index].isComplete = !todoList[index].isComplete;
-        render(todoList);
+    const toggleItem = (id) => {
+        // todoList[index].isComplete = !todoList[index].isComplete;
+        const currentItem = todoList.find(item => item.id === id);
+        currentItem.isComplete = !currentItem.isComplete;
+        fetch(`http://localhost:3000/todolist/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(currentItem)
+        })
+        .then(res => res.json())
+        .then(json => {
+            todoList[todoList.findIndex(item => item.id === id)] = json;
+            render(todoList);
+        })
     }
-    const removeItem = (index) => {
-        console.log(index);
-        todoList.splice(index, 1);
-        console.log(todoList);
-        render(todoList);
+    const removeItem = (id) => {
+        fetch(`http://localhost:3000/todolist/${id}`, {
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(json => {
+            todoList = todoList.filter(item => item.id !== id);
+            render(todoList);
+        });
+        // todoList.splice(index, 1);
     }
 
     // 綁定 input keydown 事件，按下 Enter 後 addItem 並清空 input
@@ -39,10 +81,10 @@
         const target = event.target;
         if(target && (target.matches('a.unfinish') || target.matches('a.finish'))) {
             // 按下了 check button，進行 toggle
-            toggleItem(target.dataset.index)
+            toggleItem(parseInt(target.dataset.id, 10))
         } else if(target && target.matches('a.del')) {
             // 按下了 del button，進行 remove
-            removeItem(target.dataset.index)
+            removeItem(parseInt(target.dataset.id, 10))
         }
     });
 
@@ -55,11 +97,11 @@
     // render 所有 todo item
     function renderTodoList(todoList) {
         const html = todoList.map((item, index) => `<li class="list">
-                <a class="${item.isComplete ? 'finish' : 'unfinish'}" data-index=${index}></a>
+                <a class="${item.isComplete ? 'finish' : 'unfinish'}" data-id=${item.id}></a>
                 <p class="unfinishName">
                     ${item.name}
                 </p>
-                <a class="del" data-index=${index}></a>
+                <a class="del" data-id=${item.id}></a>
             </li>`).join('')
         todoListDOM.innerHTML = html;
     }
